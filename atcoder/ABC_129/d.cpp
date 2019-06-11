536c6059eec156d249a8cd138e91c7782e3622a8
odrum428 <oxkeita@gmail.com>
2019-06-12 01:15:27 +0900

    abc_129のdをac

--- atcoder/ABC_129/d.cpp
+++ atcoder/ABC_129/d.cpp
@@ -4,61 +4,58 @@ using namespace std;
 char floormap[2000][2000] = {};
 int H, W;
 
-int count_lamp_ue(int th, int tw) {
-    int ret = 0;
+int Umap[2000][2000] = {};
+int Rmap[2000][2000] = {};
+int Smap[2000][2000] = {};
+int Lmap[2000][2000] = {};
 
-    // 上を見に行く
-    for(int i = (th-1); 0 <= i; --i) {
-        if (floormap[i][tw] == '#') break;
-        ++ret;
+// 事前にその場所でランプを照らせる箇所を計算する
+void count_lamp_ue() {
+// その場所から上にどれだけ移動できるか
+
+    for (int i = 0; i < H; ++i) {
+        for(int j = 0; j < W; ++j) {
+            if (floormap[i][j] == '#') Umap[i][j] = 0;
+            else if (i == 0) Umap[i][j] = 1;
+            else Umap[i][j] = Umap[i-1][j] + 1;
+        }
     }
-    return ret;
 }
 
-int count_lamp_sita(int th, int tw)
-{
-    int ret = 0;
-    // 下を見に行く
-    // cout << th << H;
-    for (int j = th; j < H; ++j) {
-        // cout << floormap[j][tw];
-        if (floormap[j][tw] == '#') break;
-        ++ret;
+bool count_lamp_sita() {
+// その場所から下にどれだけ移動できるか
+    for (int i = H; i > 0; --i) {
+        for(int j = 0; j < W; ++j) {
+            if (floormap[i][j] == '#') Smap[i][j] = 0;
+            else if (i == H) Smap[i][j] = 1;
+            else Smap[i][j] = Smap[i+1][j] + 1;
+        }
     }
+}
 
-    return ret;
+bool count_lamp_migi() {
+// その場所から右にどれだけ移動できるか
+    for (int i = 0; i < H; ++i) {
+        for(int j = W; j > 0; --j) {
+            if (floormap[i][j] == '#') Rmap[i][j] = 0;
+            else if (j == W) Rmap[i][j] = 1;
+            else Rmap[i][j] = Rmap[i][j+1] + 1;
+        }
+    }
+}
+
+bool count_lamp_hidari() {
+// その場所から左にどれだけ移動できるか
+    for (int i = 0; i < H; ++i) {
+        for(int j = 0; j < W; ++j) {
+            if (floormap[i][j] == '#') Lmap[i][j] = 0;
+            else if (j == 0) Lmap[i][j] = 1;
+            else Lmap[i][j] = Lmap[i][j-1] + 1;
+        }
+    }
 }
 
 
-int count_lamp_migi(int th, int tw)
-{
-    int ret = 0;
-
-    // 右を見に行く
-    for (int i = (tw+1); i < W; ++i)
-    {
-        if (floormap[th][i] == '#')
-            break;
-        ++ret;
-    }
-
-    return ret;
-}
-
-int count_lamp_hidari(int th, int tw)
-{
-    int ret = 0;
-
-    // // 左を見に行く
-    for (int i = (tw-1); 0 <= i; --i)
-    {
-        if (floormap[th][i] == '#')
-            break;
-        ++ret;
-    }
-
-    return ret;
-}
 int main()
 {
     cin >> H >> W;
@@ -72,13 +69,18 @@ int main()
         }
     }
 
+    count_lamp_ue();
+    count_lamp_sita();
+    count_lamp_migi();
+    count_lamp_hidari();
+
     for (int i = 0; i < H; ++i)
     {
         for (int j = 0; j < W; ++j)
         {
             int tmp = 0;
             if (floormap[i][j] == '#') continue;
-            tmp = count_lamp_ue(i, j) + count_lamp_sita(i, j) + count_lamp_migi(i, j) +  count_lamp_hidari(i, j);
+            tmp = Umap[i][j] + Umap[i][j] + Rmap[i][j] + Lmap[i][j] - 3;
             if (tmp > ans)  {
                 ans = tmp;
                 continue;
