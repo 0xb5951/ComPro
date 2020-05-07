a1fcc6cd183840603b20af09b1b89219fdb8aa9d
odrum428 <oxkeita@gmail.com>
2020-05-07 16:58:48 +0900

    add stange_bank

---
+++ algo_practice/DP/strange_bank.py
@@ -0,0 +1,22 @@
+N = int(input())
+count = 0
+
+values = [1]
+# 金額のパターンを設定
+for i in range(1, int(N/9)):
+    values.append(6**i)
+    values.append(9**i)
+
+values.sort()
+
+# dpの初期化
+dp =  [99999999] * (N+1)
+dp[0] = 0
+dp[1] = 1
+
+for i in range(1, N+1):
+    for j in values:
+        if i - j > 0:
+            dp[i] = min(dp[i], dp[i-j]+1)
+
+print(dp[N])
